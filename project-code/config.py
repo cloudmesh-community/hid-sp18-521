e436e1f8f62f357424919082ebc92ad2657724d8
Gregor von Laszewski <laszewski@gmail.com>
2018-04-08 04:50:49 -0400

    Update config.py

--- project-code/config.py
+++ project-code/config.py
@@ -1,2 +1,22 @@
 mysql_user = 'IUuser'
 mysql_user_password = 'Password123'
+
+'''
+
+pseudo code suggested by gregor
+
+if there is no file at ~/.cloudmesh/tbd-config.yaml
+   copy the file there from etc
+   
+read in the yaml
+changed = false
+if passwd == TBD
+   get a new password
+   changed = true
+if username == TBD 
+   get a new username
+   changed = true
+if changed:
+   write the yaml file back to  ~/.cloudmesh/tbd-config.yaml
+
+'''
