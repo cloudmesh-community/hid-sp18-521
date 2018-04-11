654d264574bdd8809c0dc88591fa4f29b318eefd
srsteinb <srsteinb@iu.edu>
2018-04-11 15:11:01 -0500

    added yaml config file

--- project-code/aws-data-services.py
+++ project-code/aws-data-services.py
@@ -1,21 +1,32 @@
-import boto3, smart_open, pprint, pymysql, config, urllib, json, decimal, psycopg2
+import boto3, smart_open, pprint, pymysql, urllib, json, decimal, psycopg2, yaml
 from boto3.dynamodb.conditions import Key, Attr
 
-# AWS credentials stored in ~/.aws/credentials, connect using IAM user
+with open ("etc/aws-data-services.yml", 'r') as ymlfile:
+    config = yaml.load(ymlfile)
+
+mysql_user = config['cloudmesh']['aws-data-services']['mysql']['user']['name']
+mysql_password = config['cloudmesh']['aws-data-services']['mysql']['user']['password']
+redshift_user = config['cloudmesh']['aws-data-services']['redshift']['user']['name']
+redshift_password = config['cloudmesh']['aws-data-services']['redshift']['user']['password']
+ACCESS_KEY_ID = config['cloudmesh']['aws-data-services']['aws']['ACCESS_KEY_ID']
+SECRET_ACCESS_KEY = config['cloudmesh']['aws-data-services']['aws']['SECRET_ACCESS_KEY']
+
 # S3 bucket name = hid-sp18-521
 
 # https://data.medicare.gov/Hospital-Compare/Patient-survey-HCAHPS-Hospital/dgck-syfz
 
 # Pulls Medicare hospital survey data set in CSV format directly from their web site into an S3 bucket
 def medicare_patient_survey_data_csv_to_s3():
-    with smart_open.smart_open('s3://hid-sp18-521/PatientSurveyData.csv', 'wb') as fout:
+    with smart_open.smart_open('s3://' + ACCESS_KEY_ID + ':' + SECRET_ACCESS_KEY +'@hid-sp18-521/PatientSurveyData.csv', 'wb') as fout:
         for line in smart_open.smart_open('https://data.medicare.gov/resource/rmgi-5fhi.csv'):
             response = fout.write(line + '\n')
             return response
 
+medicare_patient_survey_data_csv_to_s3()
+
 # Pulls Medicare patient survey data set in JSON format directly from their web site into an S3 bucket
 def medicare_patient_survey_data_json_to_s3():
-    with smart_open.smart_open('s3://hid-sp18-521/PatientSurveyData.json', 'wb') as fout:
+    with smart_open.smart_open('s3://' + ACCESS_KEY_ID + ':' + SECRET_ACCESS_KEY +'@hid-sp18-521/PatientSurveyData.json', 'wb') as fout:
         for line in smart_open.smart_open('https://data.medicare.gov/resource/rmgi-5fhi.json'):
             response = fout.write(line + '\n')
             return response
@@ -24,7 +35,7 @@ def medicare_patient_survey_data_json_to_s3():
 def s3_bucket_allfiles():
     file_names = []
 
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
     bucket = s3.Bucket('hid-sp18-521')
 
     for object in bucket.objects.all():
@@ -34,7 +45,7 @@ def s3_bucket_allfiles():
 
 # Import S3 File into RDS using AWS Data Pipeline (show how it was created and how it can be called from here)
 def data_pipeline_s3_to_rds():
-    pipeline = boto3.client('datapipeline', region_name='us-east-1')
+    pipeline = boto3.client('datapipeline', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
     response = pipeline.activate_pipeline(pipelineId='df-09855991V8LTRRRNJOQW')
 
@@ -42,7 +53,7 @@ def data_pipeline_s3_to_rds():
 
 # Return the runtime status of the S3 to RDS data pipelines
 def data_pipeline_s3_to_rds_status():
-    pipeline = boto3.client('datapipeline', region_name='us-east-1')
+    pipeline = boto3.client('datapipeline', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
     pipeline_status = pipeline.describe_pipelines(pipelineIds=['df-09855991V8LTRRRNJOQW'])
 
@@ -55,8 +66,8 @@ def data_pipeline_s3_to_rds_status():
 # Query the table that contains the data we imported into MySQL on RDS from S3
 def query_mysql_data():
     connection = pymysql.connect(host='iu-sp18.cgnrvgmckfic.us-east-1.rds.amazonaws.com',
-                                 user=config.mysql_user,
-                                 password=config.mysql_user_password,
+                                 user=mysql_user,
+                                 password=mysql_password,
                                  db='I524',
                                  charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor)
@@ -69,7 +80,7 @@ def query_mysql_data():
 
 # Delete the DynamoDB table PatientSurveyData
 def dynamodb_delete_table():
-    dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+    dynamodb = boto3.client('dynamodb', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
     response = dynamodb.delete_table(TableName='PatientSurveyData')
 
@@ -77,7 +88,7 @@ def dynamodb_delete_table():
 
 # Create the DynamoDB table PatientSurveyData
 def dynamodb_create_table():
-    dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+    dynamodb = boto3.client('dynamodb', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
     response = dynamodb.create_table(TableName='PatientSurveyData',
                                   KeySchema=[{'AttributeName': 'provider_id', 'KeyType': 'HASH'}, ],
                                   AttributeDefinitions=[{'AttributeName': 'provider_id', 'AttributeType': 'S'}],
@@ -87,7 +98,7 @@ def dynamodb_create_table():
 
 #Import JSON file from web and into DynamoDB table
 def dynamodb_insert_json_file():
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    dynamodb = boto3.resource('dynamodb', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
     table = dynamodb.Table('PatientSurveyData')
 
@@ -102,7 +113,7 @@ def dynamodb_insert_json_file():
 
 # Query the data set we just inserted into DymamoDB (Get all hospitals from the survey located in Missouri)
 def dynamodb_query_data():
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    dynamodb = boto3.resource('dynamodb', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
     table = dynamodb.Table('PatientSurveyData')
 
@@ -115,12 +126,12 @@ def dynamodb_query_data():
 # Import CSV data into Redshift cluster from S3 CSV file (created in Management Console and SQL Workbench J)
 def redshift_load_table_from_s3():
     redshift = psycopg2.connect(dbname= 'i524', host='iu-sp18.c9tuimcojmsj.us-east-1.redshift.amazonaws.com',
-                                port= '5439', user= 'iuuser', password= 'Password123')
+                                port= '5439', user= redshift_user, password= redshift_password)
 
     cur = redshift.cursor()
 
     cur.execute("COPY PatientSurveyData FROM 's3://hid-sp18-521/PatientSurveyData.csv' "
-                "ACCESS_KEY_ID '" + config.ACCESS_KEY_ID + "' SECRET_ACCESS_KEY '" + config.SECRET_ACCESS_KEY + "' "
+                "ACCESS_KEY_ID '" + ACCESS_KEY_ID + "' SECRET_ACCESS_KEY '" + SECRET_ACCESS_KEY + "' "
                 "ignoreheader 1 "
                 "removequotes "
                 "delimiter ',';")
@@ -130,7 +141,7 @@ def redshift_load_table_from_s3():
 # Clear out all data from the Redshift table PatientSurveyData
 def redshift_delete_table_data():
     redshift = psycopg2.connect(dbname= 'i524', host='iu-sp18.c9tuimcojmsj.us-east-1.redshift.amazonaws.com',
-                                   port= '5439', user= 'iuuser', password= 'Password123')
+                                   port= '5439', user= redshift_user, password= redshift_password)
 
     cur = redshift.cursor()
 
@@ -141,7 +152,7 @@ def redshift_delete_table_data():
 # Query data from the Redshift table PatientSurveyData TODO: Change this to a useful query
 def redshift_query_table_data():
     redshift = psycopg2.connect(dbname= 'i524', host='iu-sp18.c9tuimcojmsj.us-east-1.redshift.amazonaws.com',
-                                   port= '5439', user= 'iuuser', password= 'Password123')
+                                   port= '5439', user= redshift_user, password= redshift_password)
 
     cur = redshift.cursor()
 
@@ -150,7 +161,3 @@ def redshift_query_table_data():
     redshift.commit()
 
     return sql
-
-redshift_load_table_from_s3()
-
-pprint.pprint(redshift_query_table_data())
--- project-code/config.py
+++
@@ -1,5 +0,0 @@
-mysql_user = 'IUuser'
-mysql_user_password = 'Password123'
-
-ACCESS_KEY_ID = 'AKIAIHU7MHYUPJLSCUMQ'
-SECRET_ACCESS_KEY = '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'
